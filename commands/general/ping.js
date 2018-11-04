2aab052c6b6db5db9f3dd42710efcff7736cba2e
Bryan3838 <bryantran78@gmail.com>
2018-11-03 19:12:12 -0700

    error logging

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -1,10 +1,14 @@
 
 exports.exec = function (client, message, args) {
-    let ping = new Date();
-    message.channel.send("Pong!").then(resultMessage => {
-        let pong = Math.floor(new Date() - ping);
-        resultMessage.edit(`Pong! \`${pong}ms\``);
-    });
+    try {
+        let ping = new Date();
+        message.channel.send("Pong!").then(resultMessage => {
+            let pong = Math.floor(new Date() - ping);
+            resultMessage.edit(`Pong! \`${pong}ms\``);
+        });
+    } catch (err) {
+        message.client.log.error(err);
+    }
 }
 
 exports.help = {
