9d5a71077eca4e856723c86aa399544649202727
Bryan3838 <bryantran78@gmail.com>
2018-11-04 16:14:58 -0800

    minimize file reads

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,72 +1,91 @@
 const fs = require('fs');
 const path = require('path');
-const color = require('chalk');
+const COLOR = require('chalk');
 const { RichEmbed } = require('discord.js');
 
 exports.exec = async function (client, message, args) {
     try {
         message.reply('I\'m sending you the list of commands in your direct messages.');
 
-        let modules = fs.readdirSync('./commands').filter(file => fs.statSync(path.join('./commands/', file)).isDirectory());
+        // Add help RichEmbed to discord client object to minimize file reads.
+        if (!message.client.help.commands || !message.client.help.links) {
+            let modules = fs.readdirSync('./commands').filter(file => fs.statSync(path.join('./commands/', file)).isDirectory());
 
-        let loadTime = new Date();
+            let loadTime = new Date();
 
-        let general = '';
-        let moderation = '';
-        let music = '';
+            let general = '';
+            let moderation = '';
+            let music = '';
+            let settings = '';
 
-        for (let module of modules) {
-            let commandFiles = fs.readdirSync(path.resolve('./commands/', module)).
-                filter(file => !fs.statSync(path.resolve('./commands/', module, file)).isDirectory()).
-                filter(file => file.endsWith('.js'));
-            for (let file of commandFiles) {
-                file = file.replace(/\.js$/i, '');
-                if (module == 'general') {
-                    general += `• !${file}\n`;
-                }
-                else if (module == 'moderation') {
-                    moderation += `• !${file}\n`;
-                }
-                else if (module == 'music') {
-                    music += `• !${file}\n`;
+            for (let module of modules) {
+                let commandFiles = fs.readdirSync(path.resolve('./commands/', module)).
+                    filter(file => !fs.statSync(path.resolve('./commands/', module, file)).isDirectory()).
+                    filter(file => file.endsWith('.js'));
+                for (let file of commandFiles) {
+                    file = file.replace(/\.js$/i, '');
+                    if (module == 'general') {
+                        general += `• !${file}\n`;
+                    } else if (module == 'moderation') {
+                        moderation += `• !${file}\n`;
+                    } else if (module == 'music') {
+                        music += `• !${file}\n`;
+                    } else if (module == 'settings') {
+                        settings += `• !${file}\n`;
+                    }
                 }
             }
+            //temp
+            moderation = "lol";
+            music = "lol";
+            settings = "lol"
+            //
+            loadTime = Math.floor(new Date() - loadTime);
+
+            let embed = new RichEmbed()
+                .setAuthor('Commands', message.guild.iconURL)
+                .setColor('#36393f')
+                .addField('General', general)
+                .addField('Moderation', moderation)
+                .addField('Music', music)
+                .addField('Settings', settings)
+                .setFooter(`commands: ${message.client.commands.size} | Prefix: | Took ${loadTime}ms`, message.author.displayAvatarURL)
+            await message.author.send({ embed });
+            message.client.help.commands = { embed };
+
+            let link = await client.generateInvite(["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_CHANNELS",
+            "MANAGE_GUILD", "ADD_REACTIONS", "READ_MESSAGES", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", 
+            "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "USE_EXTERNAL_EMOJIS", "CONNECT", "SPEAK", "MUTE_MEMBERS",
+            "DEAFEN_MEMBERS", "MOVE_MEMBERS", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS"]);
+
+            embed = new RichEmbed()
+                .setAuthor('Additional Links')
+                .setColor('#36393f')
+                .setDescription(`[Bot Invite](${link})`)
+            await message.author.send({ embed });
+            message.client.help.links = { embed };
+
+            } else {
+                await message.author.send(message.client.help.commands);
+                await message.author.send(message.client.help.links);
+            }
+
+        } catch(err) {
+            message.client.log.error(err);
         }
-        //temp
-        moderation = "lol";
-        music = "lol";
-        //
-        loadTime = Math.floor(new Date() - loadTime);
-
-        let embed = new RichEmbed()
-            .setAuthor('Commands', message.guild.iconURL)
-            .setColor('#36393f')
-            .addField('General', general)
-            .addField('Moderation', moderation)
-            .addField('Music', music)
-            .setFooter(`commands: ${message.client.commands.size} | Prefix: | Took ${loadTime}ms`, message.author.displayAvatarURL)
-        await message.author.send({ embed });
-
-        let link = await client.generateInvite(["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_CHANNELS",
-        "MANAGE_GUILD", "ADD_REACTIONS", "READ_MESSAGES", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", 
-        "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "USE_EXTERNAL_EMOJIS", "CONNECT", "SPEAK", "MUTE_MEMBERS",
-        "DEAFEN_MEMBERS", "MOVE_MEMBERS", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS"]);
-
-        embed = new RichEmbed()
-            .setAuthor('Additional Links')
-            .setColor('#36393f')
-            .setDescription(`[Bot Invite](${link})`)
-        await message.author.send({embed});
-
-    } catch(err) {
-        message.client.log.error(err);
-    }
 }
 
 exports.help = {
-    "name": "help"
+    name: 'help',
+    description: '',
+    userTextPermission: '',
+    userVoicePermission: '',
+    usage: '',
+    example: [],
 }
 
 exports.config = {
-    "aliases": []
+    aliases: [],
+    enabled: true,
+    argsDefinitions: {}
 }
