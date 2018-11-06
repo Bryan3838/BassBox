cc25af4a080ad9226944432bcafcbd7023525cd6
Bryan3838 <bryantran78@gmail.com>
2018-11-05 21:24:14 -0800

    parse arguments

---
+++ methods/parseArgs.js
@@ -0,0 +1,18 @@
+module.exports = async (command, args) => {
+    let missingArgs = [];
+    let parsed = {};
+
+    let argsDefinitions = command.config.argsDefinitions
+    for (let i = 0; i < argsDefinitions.length; i++) {
+        if (!argsDefinitions[i].optional && !args[i]) {
+            missingArgs.push(argsDefinitions[i].name);
+        }
+        parsed[argsDefinitions[i].name] = args[1];
+    }
+
+    if (missingArgs.length > 0) {
+        parsed['missing'] = missingArgs;
+    } else parsed['missing'] = false;
+
+    return parsed;
+}
